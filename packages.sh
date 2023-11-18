0ea0af3503f70092c5f81e5f283cb7d1b685c2e0
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-11-18 13:25:49 -0600

    Add files via upload

---
+++ packages.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+echo 'Hello World'
+
+yay -S postman-bin
+yay -S slack-desktop
+
+
+sudo pacman -S alacritty
+sudo pacman -S zsh-theme-powerlevel10k
+sudo pacman -S pycharm-community-edition
+sudo pacman -S intellij-idea-community-edition
+sudo pacman -S dbeaver
+sudo pacman -S discord
+sudo pacman -S steam
+sudo pacman -S libreoffice-still-en-za
