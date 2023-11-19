490b41fc0c9cf89f0d09cb79c80e19d980d1ba24
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-11-19 16:05:28 -0600

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -12,6 +12,7 @@ echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zs
 sudo pacman -S alacritty
 sudo pacman -S pycharm-community-edition
 sudo pacman -S intellij-idea-community-edition
+sudo pacman -S gradle
 sudo pacman -S dbeaver
 sudo pacman -S discord
 sudo pacman -S steam
