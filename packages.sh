3d70f085d2f50aa36051c66a63da63994ed3a10c
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-11-18 19:56:51 -0600

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 echo 'Installing packages'
 
+mkdir /home/robert/Development
+mkdir /home/robert/Development/venv
+mkdir /home/robert/Wallpapers
 yay -S postman-bin
 yay -S slack-desktop
-
 
 sudo pacman -S alacritty
 sudo pacman -S zsh-theme-powerlevel10k
@@ -13,3 +15,4 @@ sudo pacman -S dbeaver
 sudo pacman -S discord
 sudo pacman -S steam
 sudo pacman -S libreoffice-still-en-za
+sudo pacman -S python-virtualenv
