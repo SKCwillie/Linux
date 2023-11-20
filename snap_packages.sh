fa04a247b5c9f9d4879552cb65fb784afd6d014c
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-11-20 10:17:30 -0600

    Add files via upload

---
+++ snap_packages.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+echo 'Installing packages'
+mkdir /home/robert/Development
+mkdir /home/robert/Development/venv
+mkdir /home/robert/Pictures
+mkdir /home/robert/Pictures/Wallpapers
+
+snap install pycharm-community
+snap install intellij-idea-community
+snap install gradle
+snap install dbeaver-ce
+snap install postman
+snap install slack
+snap install alacritty
+snap install discord
+snap install steam
+snap install spotify
+snap install libreoffice
+sudo apt install python3-pip
+sudo pip3 install virtualenv
