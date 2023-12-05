13776544ff2e8ec83e441c10882dd3e91d9e350b
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-12-04 21:28:26 -0600

    Update packages.sh

--- Debian/packages.sh
+++ Debian/packages.sh
@@ -9,18 +9,19 @@ echo 'Updating'
 sudo apt upgrade
 
 echo 'Installing apt packages'
-sudo apt install steam
 sudo apt install zsh
 sudo apt install python3-pip
 pip3 install virtualenv
 
 echo 'Installing Snap packages'
+snap install chromium
 snap install pycharm-community
 snap install intellij-idea-community
 snap install dbeaver-ce
 snap install postman
 snap install slack
 snap install alacritty --classic
+sudo apt install steam
 snap install discord
 snap install spotify
 snap install libreoffice
