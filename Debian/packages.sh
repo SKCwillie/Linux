aac765f8e74f5c072bffe2e3d2ba647ee0755bfa
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-12-04 23:21:02 -0600

    Update packages.sh

--- Debian/packages.sh
+++ Debian/packages.sh
@@ -5,12 +5,18 @@ mkdir /home/robert/Development
 mkdir /home/robert/Development/venv
 mkdir /home/robert/Pictures/Wallpapers
 
-echo 'Updating'
-sudo apt-get update
+echo 'Prepping'
+sudo dpkg --add-architecture i386
+sudo apt install libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+sudo apt update
 sudo apt upgrade
 
 echo 'Installing apt packages'
+sudo apt install snapd
+sudo apt install radeontop
+sudo apt install steam-insatller
 sudo apt install zsh
+sudo apt install alacritty
 sudo apt install python3-pip
 pip3 install virtualenv
 
@@ -20,8 +26,5 @@ snap install intellij-idea-community --classic
 snap install dbeaver-ce
 snap install postman
 snap install slack
-snap install alacritty --classic
-sudo apt install steam
 snap install discord
 snap install spotify
-snap install libreoffice
