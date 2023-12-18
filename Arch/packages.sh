3e069ff0ecafa4e3c68f8ab1a375239121ef39c6
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-12-17 21:13:48 -0600

    Update packages.sh

--- Arch/packages.sh
+++ Arch/packages.sh
@@ -26,6 +26,7 @@ sudo pacman -S intellij-idea-community-edition
 sudo pacman -S dbeaver
 sudo pacman -S dpkg
 sudo pacman -S nitrogen
+sudo pacman -S rofi
 sudo pacman -S libreoffice-still-en-za
 sudo pacman -S python-virtualenv
 sudo pacman -S spotify
