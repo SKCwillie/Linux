2e69c4f994cb1caf4ab92acbe4beb83c1354436e
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-12-20 18:52:20 +0000

    Update steam.sh

--- Arch/steam.sh
+++ Arch/steam.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 #enable multilib
-#sudo sed  's/#[multilib]/[multilib]/' /etc/pacman.conf
-#sudo sed  "s@#Include = /etc/pacman.d/mirrorlist@Include = /etc/pacman.d/mirrorlist@" /etc/pacman.conf
-sed "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sudo sed  "s/#\\[multilib\\]/[multilib]/" /etc/pacman.conf
+sudo sed  "s@#Include = /etc/pacman.d/mirrorlist@Include = /etc/pacman.d/mirrorlist@" /etc/pacman.conf
