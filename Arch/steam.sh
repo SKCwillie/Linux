772e181441c3e65b68f3572640de422a4385a81f
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2023-12-20 19:14:52 +0000

    Update steam.sh

--- Arch/steam.sh
+++ Arch/steam.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 echo 'Enabling multilib repos'
-sudo sed -i "s@#\[multilib\]@[multilib]@" /etc/pacman.conf
-sudo sed -i "s@#Include = /etc/pacman.d/mirrorlist@Include = /etc/pacman.d/mirrorlist@" /etc/pacman.conf
+#sudo sed -i "s@#\[multilib\]@[multilib]@" /etc/pacman.conf
+sed "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
-echo 'Installing Drivers'
-sudo pacman -S mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
+# echo 'Installing Drivers'
+# sudo pacman -S mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
 
-echo 'Installing Steam'
-sudo pacman -S steam
+# echo 'Installing Steam'
+# sudo pacman -S steam
