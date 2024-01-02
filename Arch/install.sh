70ea5c47e103830af09f866db9c54229219d82c3
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2024-01-01 22:56:20 -0600

    Update install.sh

--- Arch/install.sh
+++ Arch/install.sh
@@ -3,7 +3,13 @@
 echo "Downloading packages"
 sudo pacman -Syu
 sudo pacman -S xorg
-sudo pacman -S plasma-meta kde-applications
+#KDE
+#sudo pacman -S plasma-meta kde-applications
+
+#i3
+sudo pacman -S i3-wm
+echo "exec i3" >> ~/.xinitrc
+startx
 
 echo "Enabling services"
 sudo systemctl enable sddm
