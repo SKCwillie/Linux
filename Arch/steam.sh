6803afd5273b0b06f65839830f7a1fd59425122e
Robert Williams <64146253+SKCwillie@users.noreply.github.com>
2024-01-05 21:55:24 -0600

    Update steam.sh

--- Arch/steam.sh
+++ Arch/steam.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
-echo 'Enabling multilib repos'
-sed "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+# Enable multilib repos at /etc/pacman.conf
 
 echo 'Installing Drivers'
-sudo pacman -S mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
+sudo pacman -S mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32va-mesa-driver
 
 echo 'Installing Steam'
 sudo pacman -S steam
